475b78bccd990544025bcd165a0db6ee26cae4c7
SimrithaReddy <simrithareddyk04@gmail.com>
2022-12-21 21:00:58 +0530

    sign in done

--- backend/server.js
+++ backend/server.js
@@ -2,31 +2,25 @@ const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require('dotenv')
 const cors = require('cors')
-const addProperty = require('./routes/addproperty')
-
+const addProperty = require('./routes/addproperty');
+const loginPage = require('./routes/login');
+const registerPage = require('./routes/register')
 dotenv.config();
-
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/", async (req, res) => {
-  console.log(req.url)
-  res.send("Hola")
-})
-
-
-app.use("/posts",addProperty)
+app.use("/posts",addProperty);
+app.use('/login',loginPage);
+app.use('/register',registerPage)
 
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log("connected to DB");
   }
 );
-
-
 
 app.listen(process.env.PORT, () => {
   console.log("server is up at port 5000")
