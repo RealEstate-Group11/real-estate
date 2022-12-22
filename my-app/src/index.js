000c1053a996cb3e735b5dd04fcb800f7ddb4be2
kavita-shah <shahkavita095@gmail.com>
2022-12-22 19:26:43 +0530

    signin done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './addProperty/login';
+import Login from './component/Signin/Login';
+import {
+  BrowserRouter,
+  Routes, Route
+  
+} from "react-router-dom";
+import Signup from './component/Signin/Signup';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Login/>
-  </React.StrictMode>
+  <BrowserRouter>
+  <Routes>
+  <Route path="/Login" element={<Login/>}></Route>
+  <Route path="/Signup" element={<Signup/>}></Route>
+  </Routes>
+  </BrowserRouter>
 );
