906a651d12ee9c5e7508090da679106a6e3f7a35
SimrithaReddy <simrithareddyk04@gmail.com>
2022-12-22 17:52:45 +0530

    form done

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// import Header from './header/header';
+// import SideNavBar from './asidebar/aside';
+import AddProperty from './addProperty/main';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  
+    {/* <Header />
+    <SideNavBar/> */}
+    <AddProperty/>
   </React.StrictMode>
 );
