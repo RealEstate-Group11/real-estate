73284b4960ed4899f3345d2b8382402bf0c0edad
SimrithaReddy <simrithareddyk04@gmail.com>
2022-12-22 09:48:06 +0530

    authorization done

--- backend/server.js
+++ backend/server.js
@@ -5,46 +5,33 @@ const cors = require('cors')
 const addProperty = require('./routes/addproperty');
 const loginPage = require('./routes/login');
 const registerPage = require('./routes/register')
-const Getproperty=require('./routes/get')
+const Getproperty = require('./routes/get')
+const jwt = require('jsonwebtoken')
 dotenv.config();
 const app = express();
 
-app.use("/posts", (req, res, next) => {
-    try {
-        let token = req.headers.authorization;
-        if (token) {
-            jwt.verify(token, secret, function (err, decoded) {
-                if (err) {
-                    return res.status(403).json({
-                        status: "Failed",
-                        message: err.message
-                    })
-                }
-                req.user = decoded.data
-                next();
-            })
-        } else {
-            return res.status(404).json({
-                status: "Failed",
-                message: "Not authenticated user"
-            })
-        }
-    } catch (e) {
-        res.json({
-            status: "Error",
-            message: e.message
-        })
-    }
+app.use("/properties", (req, res, next) => {
+  let token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+      if (err)  return res.status(403).send("User not authorized")
+      req.user_mail = decoded
+      next();
+  })
+  }else{
+    res.send("User not authorized")
+  }
 })
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
-app.use("/posts",addProperty);
-app.use('/login',loginPage);
-app.use('/register',registerPage)
-app.use('/properties',Getproperty)
+app.use("/properties/posts", addProperty);
+app.use('/login', loginPage);
+app.use('/register', registerPage)
+app.use('/properties', Getproperty)
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true },
   () => {
     console.log("connected to DB");
