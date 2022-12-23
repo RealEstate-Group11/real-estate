754cac399faec379b5a13ff2946a89726935b95e
SimrithaReddy <95024988+SimrithaReddy@users.noreply.github.com>
2022-12-23 19:17:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,12 @@ const Getproperty = require('./routes/get')
 const jwt = require('jsonwebtoken')
 dotenv.config();
 const app = express();
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+app.use(cors(corsOptions));
 
 app.use("/properties", (req, res, next) => {
   let token = req.headers.authorization;
@@ -23,7 +29,6 @@ app.use("/properties", (req, res, next) => {
   }
 })
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
@@ -40,4 +45,4 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTo
 
 app.listen(process.env.PORT, () => {
   console.log("server is up at port 5000")
-})  
+})   
